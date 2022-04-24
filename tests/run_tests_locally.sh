9b1067712fb9996069d41395549b755519b4e2e9
dgea005 <danielgeals@gmail.com>
2022-04-24 21:04:47 +0200

    local test against pg13

--- tests/run_tests_locally.sh
+++ tests/run_tests_locally.sh
@@ -18,5 +18,14 @@ until psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE -c "select 1" > /dev/
 done
 
 # PYTHON variable comes from this script being run from the Makefile
-${PYTHON} -m coverage run -m pytest -svx tests/
+${PYTHON} -m coverage run -a -m pytest -svx tests/test_decoders.py
+${PYTHON} -m coverage run -a -m pytest -svx tests/test_utils.py
+
+# first reader test with PGPORT=5432 is for Postgres 12
+${PYTHON} -m coverage run -a -m pytest -svx --log-cli-level=INFO tests/test_reader.py
+
+# second reader test with PGPORT=5433 is for Postgres 13
+export PGPORT=5433
+${PYTHON} -m coverage run -a -m pytest -vx --log-cli-level=INFO tests/test_reader.py
+
 ${PYTHON} -m coverage report -m
